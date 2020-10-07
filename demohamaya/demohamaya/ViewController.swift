16a6d3f4a5be6584f1a0689981f2eb15105f767c
adil-nangyal <adil@sigmads.com>
2020-10-07 18:45:39 +0500

    v1

---
+++ demohamaya/demohamaya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demohamaya
+//
+//  Created by SDS on 07/10/2020.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
